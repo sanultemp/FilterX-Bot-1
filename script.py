27887e30dfc7701a3a02cf41a0671ed8a0bae60c
StarKing Develoeprs <88481114+sanultemp@users.noreply.github.com>
2021-11-02 21:07:27 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,63 +1,37 @@
 class Script(object):
 
-    START_MSG = """<b>Hy {},
-
-I'm an advanced filter bot with many capabilities!
-There is no practical limits for my filtering capacity :)
-
-See <i>/help</i> for commands and more details.</b>
+    START_MSG = """<b>Hi {},
+I'm 𝙁𝙞𝙡𝙩𝙚𝙧𝙓 bot by BΣƬΛ 𝗕𝗼𝘁𝘀 with different abilities! 🔥
+There is no any limitations for my filtering capacity. 🙃
+See <i>/help</i> for more details. 👨‍💻</b>
+➤ <p><a href="https://t.me/BetaBots">BΣƬΛ 𝗕𝗼𝘁𝘀</a></p>
 """
 
 
     HELP_MSG = """
-<i>Add me as admin in your group and start filtering :)</i>
-
-
-<b>Basic Commands;</b>
-
+<i>Add me as admin in your group and start filtering like a pro 🙂</i>
+<b>FilterX  Commands;</b>
 /start - Check if I'm alive!
 /help - Command help
 /about - Something about me!
-
-
-<b>Filter Commands;</b>
-
 <code>/add name reply</code>  -  Add filter for name
-
 <code>/del name</code>  -  Delete filter
-
 <code>/delall</code>  -  Delete entire filters (Group Owner Only!)
-
 <code>/viewfilters</code>  -  List all filters in chat
-
-
 <b>Connection Commands;</b>
-
 <code>/connect groupid</code>  -  Connect your group to my PM. You can also simply use,
 <code>/connect</code> in groups.
-
 <code>/connections</code>  -  Manage your connections.
-
-
-<b>Extras;</b>
-
 /status  -  Shows current status of your bot (Auth User Only)
-
 /id  -  Shows ID information
-
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
-
-
-<b>© @TroJanzHEX</b>
+➤ <p><a href="https://t.me/BetaBots">BΣƬΛ 𝗕𝗼𝘁𝘀</a></p>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
-
-⭕️<b>Creater :</b> @TroJanzHEX    
-
-⭕️<b>Language :</b> <code>Python3</code>
-
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
-
+    ABOUT_MSG = """🔘<b>My Name : 𝙁𝙞𝙡𝙩𝙚𝙧𝙓</b>
+🔘<b>Creater :</b> @BetaBots    
+🔘<b>Language :</b> <code>Python3</code>
+🔘<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+➤ <p><a href="https://t.me/BetaBots">BΣƬΛ 𝗕𝗼𝘁𝘀</a></p>
 """
